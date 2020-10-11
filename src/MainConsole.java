90c07f3a0a16475df4bac32481da474db90eb84d
Sasha6969 <71279648+Sasha6969@users.noreply.github.com>
2020-10-11 21:29:45 +0200

    FIRST

---
+++ src/MainConsole.java
@@ -0,0 +1,14 @@
+
+
+import java.io.IOException;
+
+import View.ConsoleView;
+
+public class MainConsole {
+
+	public static void main(String[] args) throws IOException {
+		ConsoleView start = new ConsoleView();
+		start.mainMenu();
+	}
+
+}
